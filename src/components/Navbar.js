59d095d5fd92ceb487aa5b6f8442607ff80eecaa
George Marcus <george.marcus922@gmail.com>
2022-11-06 02:01:44 +0200

    Move theme toggle

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -40,7 +40,11 @@ const Navbar = ({ onThemeChange }) => {
       >
         CV
       </a>
-      <div className="flex relative text-black items-end gap-6 md:gap-2 md:ml-auto">
+    </>
+  )
+  const contactItems = (
+    <>
+      <div className="flex relative text-black items-end gap-6 md:gap-2">
         <a href="mailto:george.marcus922@gmail.com">
           <FontAwesomeIcon icon={faEnvelope} size="2x" />
         </a>
@@ -64,33 +68,39 @@ const Navbar = ({ onThemeChange }) => {
     <header>
       <nav className="px-5 py-3 border-b flex flex-col md:flex-row justify-between">
         <div className="md:h-10 flex flex-col md:flex-row w-full">
-          <div className="w-full md:w-auto flex justify-between">
+          <div className="w-full flex justify-between">
             <a href="/" className="text-3xl font-bold text-black">
               {'< '}
               George Mărcuș
               {' />'}
             </a>
-            <Classic duration={500}
-               className={classNames('text-3xl ml-3', {
-                 'text-yellow-300': theme === 'light',
-                 'text-yellow-200': theme === 'dark',
-               })}
-               onToggle={handleThemeToggle}
-               toggled={theme === 'dark'}
-            />
+            {/* Regular items */}
+            <div className="flex grow ml-4 items-center">
+              <div className="hidden md:flex gap-5">
+                {navItems}
+              </div>
+              <Classic duration={500}
+                className={classNames('text-3xl ml-5', {
+                  'text-yellow-300': theme === 'light',
+                  'text-yellow-200': theme === 'dark',
+                })}
+                onToggle={handleThemeToggle}
+                toggled={theme === 'dark'}
+              />
+              <div className="hidden md:flex ml-auto">
+                {contactItems}
+              </div>
+            </div>
             <button className="font-bold text-black md:hidden"
                     onClick={handleClick}>
               <FontAwesomeIcon icon={faBars} size="2x" />
             </button>
           </div>
-          {/* Regular items */}
-          <div className="hidden md:flex gap-5 grow ml-4 items-center">
-            {navItems}
-          </div>
           {/* Dropdown items */}
           <Collapse isOpened={isMenuOpen}>
             <div className="flex flex-col md:hidden gap-7 mt-6 text-start">
               {navItems}
+              {contactItems}
             </div>
           </Collapse>
         </div>
